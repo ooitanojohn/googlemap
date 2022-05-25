d2935031182f74da2638e7a43bc423ecb5dbf1df
ooitanojohn <qmkh733310@gmail.com>
2022-05-25 16:04:33 +0900

    マスターにもどりたいので　一時中断

--- index.ts
+++ index.ts
@@ -1,22 +1,19 @@
-/**
- * @license
- * Copyright 2019 Google LLC. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-
-let map: google.maps.Map;
-
+// Initialize and add the map
 function initMap(): void {
-  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: 0, lng: 150.644 },
-    zoom: 8,
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.036 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(
+    document.getElementById("map") as HTMLElement,
+    {
+      zoom: 4,
+      center: uluru,
+    }
+  );
+
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
   });
 }
-
-declare global {
-  interface Window {
-    initMap: () => void;
-  }
-}
-window.initMap = initMap;
-export { };
